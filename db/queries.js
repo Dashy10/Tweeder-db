321aa10404303e49329ccf070775d81e4bfc86db
Dashy10 <dashiell.lumas@gmail.com>
2017-05-20 14:04:24 -0400

    yo

--- db/queries.js
+++ db/queries.js
@@ -1,3 +1,69 @@
+var promise = require('bluebird');
+var options = {
+  promiseLib: promise
+};
 var pgp = require('pg-promise')();
 var connString = process.env.DATABASE_URL;
 var db = pgp(connString);
+
+
+////////////////////////////////////////////
+////////////// GET ONE Tweed AT A TIME/////////////
+//////////////////////////////////////////
+
+getTweed = (req,res,next) => {
+  var id = req.params.id;
+  db.one('SELECT * FROM tweeds WHERE id = $1', id)
+    .then(function(data){
+      res.status(200)
+      .json({
+        status: 'success',
+        data: data,
+        message: 'Your tweed was retrieved'
+      });
+    })
+    .catch(function(err){
+      return next(err);
+    });
+};
+
+// GET ALL TWEEDS
+getAllTweeds = (req,res,next)  => {
+
+   db.any('SELECT * FROM tweeds')
+  .then(function(data){
+    res.status(200)
+    .json({
+      status: 'success',
+      data: data,
+      message: 'Success getting all verbs'
+    });
+  })
+  .catch(function(err){
+    return next(err);
+  });
+
+};
+
+// CREATE ONE TWEED
+
+createTweed = (req,res,next) => {
+  db.none("INSERT INTO tweeds(tweed)" +"values(${tweed})", req.body)
+  .then(function(data){
+    res.status(200)
+    .json({
+      status:'success',
+      data: data,
+      message: 'You created a tweed'
+    });
+  })
+  .catch(function(err){
+    return next(err);
+  });
+};
+
+module.exports = {
+  getTweed: getTweed,
+  getAllTweeds: getAllTweeds,
+  createTweed: createTweed,
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var cors = require('cors');
+var db = require('../db/queries');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Tweeds Database' });
 });
+
+router.get('/tweeds', db.getAllTweeds);
+router.get('/tweeds/:id', db.getTweed);
+router.post('/tweeds/', db.createTweed);
 
 module.exports = router;
